a163b54c06aafb7713f6adfb353bf82a39ee9856
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-10-07 00:38:22 +0100

    feat: app startup

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { router as authRouter } from "./routes/auth";
+import { router as walletRouter } from "./routes/wallet";
+import { router as transactionRouter } from "./routes/transaction";
+import { configuration } from "./config/config";
+
+const app = express();
+const port = configuration.app_port;
+
+app.use(express.json());
+app.use("/auth", authRouter);
+app.use("/wallet", walletRouter);
+app.use("/transactions", transactionRouter);
+
+app.listen(port, () => {
+  console.log(`Connected to port:${port}`);
+});
