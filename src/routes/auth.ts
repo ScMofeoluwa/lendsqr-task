f30174c82a92de0208cbb64fb4da93de314eadfa
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-10-08 06:10:27 +0100

    feat: added validation middleware to routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,18 @@
 import { Router } from "express";
 import AuthController from "../controller/auth";
+import AuthValidator from "../validators/auth";
+import RequestBodyMiddleware from "../middleware/requestbody";
 
 const router = Router();
-router.post("/register", AuthController.create);
-router.post("/login", AuthController.login);
+router.post(
+  "/register",
+  RequestBodyMiddleware.validate(AuthValidator.register()),
+  AuthController.create,
+);
+router.post(
+  "/login",
+  RequestBodyMiddleware.validate(AuthValidator.login()),
+  AuthController.login,
+);
 
 export { router };
--- src/routes/wallet.ts
+++ src/routes/wallet.ts
@@ -1,11 +1,28 @@
 import { Router } from "express";
 import WalletController from "../controller/wallet";
 import AuthMiddleware from "../middleware/auth";
+import WalletValidator from "../validators/wallet";
+import RequestBodyMiddleware from "../middleware/requestbody";
 
 const router = Router();
 router.get("/", AuthMiddleware.authorize, WalletController.getDetails);
-router.post("/fund", AuthMiddleware.authorize, WalletController.fund);
-router.post("/transfer", AuthMiddleware.authorize, WalletController.transfer);
-router.post("/withdraw", AuthMiddleware.authorize, WalletController.withdraw);
+router.post(
+  "/fund",
+  RequestBodyMiddleware.validate(WalletValidator.fund()),
+  AuthMiddleware.authorize,
+  WalletController.fund,
+);
+router.post(
+  "/transfer",
+  RequestBodyMiddleware.validate(WalletValidator.transfer()),
+  AuthMiddleware.authorize,
+  WalletController.transfer,
+);
+router.post(
+  "/withdraw",
+  RequestBodyMiddleware.validate(WalletValidator.withdraw()),
+  AuthMiddleware.authorize,
+  WalletController.withdraw,
+);
 
 export { router };
