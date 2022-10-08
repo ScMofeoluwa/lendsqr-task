eddb45df623d54433f0cfcc42eeab7ab358b5cb1
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-10-08 11:39:46 +0100

    refactor: wallet validator

--- src/validators/wallet.ts
+++ src/validators/wallet.ts
@@ -3,22 +3,21 @@ import Joi from "joi";
 class WalletValidator {
   fund() {
     return Joi.object({
-      email: Joi.string().min(5).max(255).email().required(),
-      amount: Joi.string().required(),
+      amount: Joi.number().min(1).required(),
     });
   }
 
   transfer() {
     return Joi.object({
       username: Joi.string().min(5).max(50).required(),
-      amount: Joi.string().required(),
+      amount: Joi.number().min(1).required(),
     });
   }
 
   withdraw() {
     return Joi.object({
       name: Joi.string().min(5).max(50).required(),
-      amount: Joi.string().required(),
+      amount: Joi.number().min(1).required(),
       account: Joi.string().required(),
       reason: Joi.string().optional(),
     });
